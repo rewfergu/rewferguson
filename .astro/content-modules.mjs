72aeea51a2cff160ff2233d9e067402fcf7057d7
Robert Ferguson <rob@rewferguson.com>
2026-09-21 13:09:24 -0500

    add marketing work page

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -6,6 +6,7 @@ export default new Map([
 ["src/content/work/budget-tracker.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fbudget-tracker.mdx&astroContentModuleFlag=true")],
 ["src/content/work/flip-draw.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fflip-draw.mdx&astroContentModuleFlag=true")],
 ["src/content/work/iw-wordpress-theme.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fiw-wordpress-theme.mdx&astroContentModuleFlag=true")],
+["src/content/work/marketing-os.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fmarketing-os.mdx&astroContentModuleFlag=true")],
 ["src/content/work/notecode.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fnotecode.mdx&astroContentModuleFlag=true")],
 ["src/content/work/photo-search.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fphoto-search.mdx&astroContentModuleFlag=true")],
 ["src/content/work/sail-ui.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwork%2Fsail-ui.mdx&astroContentModuleFlag=true")],
